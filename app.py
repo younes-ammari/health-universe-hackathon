0e4e1510b286f58a10cc9c7bc02bd61d7d376fb3
Younes Ammari <retcheryounes@gmail.com>
2023-08-31 22:56:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ async def diseases():
 
 
 # Run the FastAPI app using uvicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
 
